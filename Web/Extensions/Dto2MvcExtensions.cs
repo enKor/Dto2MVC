f5119f2b5bd4ddaf38e3913cf6269736008eda32
Martin Procházka <prochazka@cngroup.dk>
2023-07-01 11:27:38 +0200

    empty extension and its usage in Program.cs

---
+++ Web/Extensions/Dto2MvcExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Extensions;
+
+public static class Dto2MvcExtensions
+{
+    public static IServiceCollection AddDto2Mvc<TController>(this IServiceCollection services,
+        params Type[] pivots)
+    where TController: Controller
+    {
+
+
+        return services;
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,32 +1,37 @@
-namespace Web
+using Web.Controllers;
+using Web.Extensions;
+using Web.Models;
+
+namespace Web;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        var services = builder.Services;
+        services.AddControllersWithViews();
+        services.AddDto2Mvc<MyControllerBase>(typeof(CarModel));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+            app.UseExceptionHandler("/Home/Error");
         }
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
