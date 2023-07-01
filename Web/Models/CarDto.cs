88f169d8b5d2089782587135b10da34ca87b9ad6
Martin Procházka <prochazka@cngroup.dk>
2023-07-01 11:10:54 +0200

    sample dto

---
+++ Web/Models/CarDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using Web.Attributes;
+
+namespace Web.Models;
+
+[Dto2Mvc(Dto2MvcAttribute.HttpMethod.Get,"Cars","Load")]
+[Dto2Mvc(Dto2MvcAttribute.HttpMethod.Get,"Cars","Index")]
+[Dto2Mvc(Dto2MvcAttribute.HttpMethod.Post,"Cars","Save")]
+public class CarModel
+{
+    public bool IsNew { get; set; }
+    public string Model { get; set; }
+    public int Length { get; set; }
+    public Color Color { get; set; }
+}
