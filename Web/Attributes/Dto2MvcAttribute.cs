a834f454dba09c290f77b5dc04d8dd94f94c0c64
Martin Procházka <prochazka@cngroup.dk>
2023-07-01 11:10:42 +0200

    Dto2MvcAttribute

---
+++ Web/Attributes/Dto2MvcAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace Web.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+public class Dto2MvcAttribute : Attribute
+{
+    public string Controller { get; }
+    public string Action { get; }
+    public HttpMethod Method { get; }
+
+    public Dto2MvcAttribute(HttpMethod method, string controller, string action)
+    {
+        Controller = controller;
+        Action = action;
+    }
+
+    public enum HttpMethod
+    {
+        Get,
+        Post
+    }
+}
